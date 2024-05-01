613a6970fc5ebda55078fc30ef276da8b5fd5dcc
Adam Williams <lol768@users.noreply.github.com>
2024-05-01 11:52:20 +0100

    Move to a re-used HttpClient for the proxying

--- FallbackSpaProxyingMiddleware.cs
+++ FallbackSpaProxyingMiddleware.cs
@@ -11,27 +11,19 @@ public class FallbackSpaProxyingMiddleware
     private readonly RequestDelegate _next;
     private readonly IHostApplicationLifetime _lifetime;
     private readonly string _baseUrl;
+    private readonly HttpClient _neverTimeOutHttpClient;
 
-    public FallbackSpaProxyingMiddleware(RequestDelegate next, IHostApplicationLifetime lifetime, string baseUrl)
+    public FallbackSpaProxyingMiddleware(RequestDelegate next, IHostApplicationLifetime lifetime, string baseUrl,
+        HttpClient neverTimeOutHttpClient)
     {
         _next = next;
         _lifetime = lifetime;
         _baseUrl = baseUrl;
+        _neverTimeOutHttpClient = neverTimeOutHttpClient;
     }
 
     public async Task Invoke(HttpContext context)
     {
-        var handler = new HttpClientHandler
-        {
-            AllowAutoRedirect = false,
-            UseCookies = false,
-        };
-
-        var neverTimeOutHttpClient = new HttpClient(handler)
-        {
-            Timeout = Timeout.InfiniteTimeSpan
-        };
-
         if (context.GetEndpoint() == null)
         {
             var spaProxyType =
@@ -44,7 +36,7 @@ public class FallbackSpaProxyingMiddleware
                 null,
                 new object[]
                 {
-                    context, neverTimeOutHttpClient, Task.FromResult(new Uri(_baseUrl)),
+                    context, _neverTimeOutHttpClient, Task.FromResult(new Uri(_baseUrl)),
                     _lifetime.ApplicationStopping, true
                 });
         }
@@ -61,6 +53,17 @@ public static class FallbackSpaProxyingMiddlewareExtensions
     public static IApplicationBuilder UseFallbackSpaProxying(
         this IApplicationBuilder builder, string baseUri)
     {
-        return builder.UseMiddleware<FallbackSpaProxyingMiddleware>(baseUri);
+        var handler = new SocketsHttpHandler
+        {
+            AllowAutoRedirect = false,
+            UseCookies = false,
+            MaxConnectionsPerServer = 10000,
+        };
+
+        var neverTimeOutHttpClient = new HttpClient(handler)
+        {
+            Timeout = Timeout.InfiniteTimeSpan,
+        };
+        return builder.UseMiddleware<FallbackSpaProxyingMiddleware>(baseUri, neverTimeOutHttpClient);
     }
 }
